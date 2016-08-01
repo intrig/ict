19d0bf841c21980e70ee7c3be6704d8f6321d7ed
Mark Beckwith <mark@intrig.com>
2016-08-01 10:28:47 -0500

    prog

--- osstream.h
+++ osstream.h
@@ -5,20 +5,28 @@ namespace ict {
 
 // output string string with 80% functionality and 20X performance over std::ostringstream, and easier to spell.
 struct osstream {
+    osstream() {}
+    osstream(const std::string & s) : x(s) {}
+    osstream(const osstream &) = delete;
+    osstream& operator=(const osstream &) = delete;
     std::string x;
-    // Move the internal string to avoid copying.  The stream is in an undefined state after this.  It is meant to be
-    // the last thing you do.
-    std::string && take() { 
-        return std::move(x);
-    }
     // Copy the current string
     std::string str() const { 
         return x;
+    }
+    // Move the internal string to avoid copying.  The stream is in an undefined state after this.  It is meant to be
+    // the last thing you do.  Call clear() after this if you want to use the string again.
+    std::string && take() { 
+        return std::move(x);
     }
     void clear() { 
         x.clear();
     }
 };
+
+// One weird trick to make sure there are no implicit conversions.
+template <typename T>
+inline osstream & operator<<(osstream &os, T x) = delete;
 
 namespace util {
     template <typename S, typename T>
@@ -40,9 +48,5 @@ inline osstream & operator<<(osstream &os, int x) { return util::append_number(o
 inline osstream & operator<<(osstream &os, unsigned long int x) { return util::append_number(os, x); }
 inline osstream & operator<<(osstream &os, long int x) { return util::append_number(os, x); }
 inline osstream & operator<<(osstream &os, long long x) { return util::append_number(os, x); }
-
-// one weird trick to make sure there are no implicit conversions
-template <typename T>
-inline osstream & operator<<(osstream &os, T x) = delete;
 
 }
