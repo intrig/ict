c7ca8c55e009d6fbcdf7f5961c74dfd741516a83
Mark Beckwith <wythe@intrig.com>
2019-12-12 15:16:20 -0600

    better naming in macros
    Signed-off-by: Mark Beckwith <wythe@intrig.com>

--- exception.h
+++ exception.h
@@ -23,17 +23,17 @@ inline std::runtime_error create_exception(const std::string & desc, const char 
 #ifdef ANDROID
 #define IT_WARN(s) \
 do { \
-    std::ostringstream os; \
-    os << s; \
-    auto e = ict::create_exception(os.str(), __FILE__, __LINE__); \
+    std::ostringstream os_warn_; \
+    os_warn_ << s; \
+    auto e = ict::create_exception(os_warn_.str(), __FILE__, __LINE__); \
     __android_log_print(ANDROID_LOG_VERBOSE, "xenon", e..what()); \
 } while (0)
 #else
 #define IT_WARN(s) \
 do { \
-    std::ostringstream os; \
-    os << s; \
-    auto e = ict::create_exception(os.str(), __FILE__, __LINE__); \
+    std::ostringstream os_warn_; \
+    os_warn_ << s; \
+    auto e = ict::create_exception(os_warn_.str(), __FILE__, __LINE__); \
     std::cerr << e.what() << '\n'; \
 } while (0)
 #endif
@@ -41,16 +41,16 @@ do { \
 // Throw a std::runtime_error with description.
 #define IT_FATAL(desc) \
 do { \
-    std::ostringstream os; \
-    os << desc; \
-    throw ict::create_exception(os.str()); \
+    std::ostringstream os_fatal_; \
+    os_fatal_ << desc; \
+    throw ict::create_exception(os_fatal_.str()); \
 } while (0)
 
 // Throw a std::runtime_error with description and source file and line number added.
 #define IT_PANIC(desc) \
 do { \
-    std::ostringstream os; \
-    os << desc; \
-    throw ict::create_exception(os.str(), __FILE__, __LINE__); \
+    std::ostringstream os_panic_; \
+    os_panic_ << desc; \
+    throw ict::create_exception(os_panic_.str(), __FILE__, __LINE__); \
 } while (0)
 
