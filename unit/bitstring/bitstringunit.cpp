fc2c9b54c78d1b40a89d6fac81737331f0faebcf
Mark Beckwith <wythe@intrig.com>
2019-12-20 10:58:30 -0600

    Moved bit iterators into bitstring
    Signed-off-by: Mark Beckwith <wythe@intrig.com>

--- unit/bitstring/bitstringunit.cpp
+++ unit/bitstring/bitstringunit.cpp
@@ -677,13 +677,13 @@ void random_copy(size_t n) {
 }
 
 void bitstring_unit::bit_iterators() {
-    bit_iterator first;
-    IT_ASSERT(first == bit_iterator());
+    bitstring::bit_iterator first;
+    IT_ASSERT(first == bitstring::bit_iterator());
     ++first;
     first++;
     --first;
     first--;
-    IT_ASSERT(first == bit_iterator());
+    IT_ASSERT(first == bitstring::bit_iterator());
 
     {
         auto bs = bitstring("@111001");
