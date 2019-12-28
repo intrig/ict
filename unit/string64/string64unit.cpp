0787c8f5075c1de641556e7e80629ec254ce8f96
Mark Beckwith <wythe@intrig.com>
2019-12-28 08:45:57 -0600

    string64
    Signed-off-by: Mark Beckwith <wythe@intrig.com>

--- unit/string64/string64unit.cpp
+++ unit/string64/string64unit.cpp
@@ -9,7 +9,7 @@ struct spaces_t {
     char const *not_spaced;
 };
 
-const std::vector<spaces_t> spacetest = {
+static std::vector<spaces_t> spacetest = {
     { "  AABBCCDDEEFF11223344556677  ", "AABBCCDDEEFF11223344556677" },
     { "AA\tBBCCDDEEFF1122334455\t6677", "AABBCCDDEEFF11223344556677" },
     { " AA BB CC DD EE FF 11 22 33 44 55 66 77 ", "AABBCCDDEEFF11223344556677" },
@@ -28,44 +28,47 @@ void string64_unit::squash() {
 }
 
 void string64_unit::string64() {
-    ict::string64 a;
-    IT_ASSERT(a.empty());
-    ict::string64 b = "24";
-    IT_ASSERT(!b.empty());
+    {
+        ict::string64 a;
+        IT_ASSERT(a.empty());
+        ict::string64 b = "24";
+        IT_ASSERT(!b.empty());
 
-    ict::string64 c(b);
-    IT_ASSERT(!c.empty());
-    IT_ASSERT(c == b);
-    IT_ASSERT(b == c);
+        ict::string64 c(b);
+        IT_ASSERT(!c.empty());
+        IT_ASSERT(c == b);
+        IT_ASSERT(b == c);
 
-    ict::string64 d("hello");
-    IT_ASSERT(d == "hello");
-    IT_ASSERT(d[0] == 'h');
-    IT_ASSERT(d[1] == 'e');
-    IT_ASSERT(d[2] == 'l');
-    IT_ASSERT(d[3] == 'l');
-    IT_ASSERT(d[4] == 'o');
-    IT_ASSERT(d[5] == '\0');
-    IT_ASSERT(d[6] == '\0');
-    IT_ASSERT(d[7] == '\0');
+        ict::string64 d("hello");
+        IT_ASSERT(d == "hello");
+        IT_ASSERT(d[0] == 'h');
+        IT_ASSERT(d[1] == 'e');
+        IT_ASSERT(d[2] == 'l');
+        IT_ASSERT(d[3] == 'l');
+        IT_ASSERT(d[4] == 'o');
+        IT_ASSERT(d[5] == '\0');
+        IT_ASSERT(d[6] == '\0');
+        IT_ASSERT(d[7] == '\0');
 
-    IT_ASSERT(d != "goodbye");
+        IT_ASSERT(d != "goodbye");
 
-    IT_ASSERT(d.value == 478560413032);
-    uint64_t value = d.value;
-    IT_ASSERT(value == 478560413032);
-    c = d;
-    IT_ASSERT(c == d);
-    IT_ASSERT(c != a);
-
+        IT_ASSERT(d.value == 478560413032);
+        uint64_t value = d.value;
+        IT_ASSERT(value == 478560413032);
+        c = d;
+        IT_ASSERT(c == d);
+        IT_ASSERT(c != a);
+    }
     {
         ict::string64 a = "";
         IT_ASSERT(a.empty());
 
         try {
             a = "way to long";
-        } catch (std::exception & e) {
-            IT_ASSERT(std::string(e.what()).find("way to long is to long for a string64") == std::string::npos);
+        } catch (std::exception &e) {
+            IT_ASSERT(std::string(e.what()).find(
+                          "way to long is to long for a string64") ==
+                      std::string::npos);
         }
     }
 
